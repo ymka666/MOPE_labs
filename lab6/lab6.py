5f6e97a4d5a224767333bb2c0f99fe6d988bdab2
ymka666 <66619758+ymka666@users.noreply.github.com>
2021-05-09 14:45:15 +0300

    Add files via upload

---
+++ lab6/lab6.py
@@ -0,0 +1,196 @@
+from _decimal import Decimal
+from scipy.stats import f, t
+from itertools import compress
+from functools import reduce
+import numpy
+import math
+import random
+
+xmin = [20, 5, 20]
+xmax = [70, 40, 45]
+
+norm_plan_raw = [[-1, -1, -1],
+                 [-1, +1, +1],
+                 [+1, -1, +1],
+                 [+1, +1, -1],
+                 [-1, -1, +1],
+                 [-1, +1, -1],
+                 [+1, -1, -1],
+                 [+1, +1, +1],
+                 [-1.73, 0, 0],
+                 [+1.73, 0, 0],
+                 [0, -1.73, 0],
+                 [0, +1.73, 0],
+                 [0, 0, -1.73],
+                 [0, 0, +1.73]]
+
+x0 = [(xmax[_] + xmin[_])/2 for _ in range(3)]
+dx = [xmax[_] - x0[_] for _ in range(3)]
+
+natur_plan_raw = [[xmin[0],           xmin[1],           xmin[2]],
+                  [xmin[0],           xmin[1],           xmax[2]],
+                  [xmin[0],           xmax[1],           xmin[2]],
+                  [xmin[0],           xmax[1],           xmax[2]],
+                  [xmax[0],           xmin[1],           xmin[2]],
+                  [xmax[0],           xmin[1],           xmax[2]],
+                  [xmax[0],           xmax[1],           xmin[2]],
+                  [xmax[0],           xmax[1],           xmax[2]],
+                  [-1.73*dx[0]+x0[0], x0[1],             x0[2]],
+                  [1.73*dx[0]+x0[0],  x0[1],             x0[2]],
+                  [x0[0],             -1.73*dx[1]+x0[1], x0[2]],
+                  [x0[0],             1.73*dx[1]+x0[1],  x0[2]],
+                  [x0[0],             x0[1],             -1.73*dx[2]+x0[2]],
+                  [x0[0],             x0[1],             1.73*dx[2]+x0[2]],
+                  [x0[0],             x0[1],             x0[2]]]
+
+
+def equation_of_regression(x1, x2, x3, cef, importance=[] * 11):
+    factors_array = [1, x1, x2, x3, x1 * x2, x1 * x3, x2 * x3, x1 * x2 * x3, x1 ** 2, x2 ** 2, x3 ** 2]
+    return sum([el[0] * el[1] for el in compress(zip(cef, factors_array), importance)])
+
+
+def func(x1, x2, x3):
+    coeffs = [3.1, 6.3, 9.8, 5.5, 2.5, 0.4, 1, 3.5, 0.7, 7.9, 8.7]
+    return equation_of_regression(x1, x2, x3, coeffs)
+
+
+def generate_factors_table(raw_array):
+    raw_list = [row + [row[0] * row[1], row[0] * row[2], row[1] * row[2], row[0] * row[1] * row[2]] + list(
+        map(lambda x: x ** 2, row)) for row in raw_array]
+    return list(map(lambda row: list(map(lambda el: round(el, 3), row)), raw_list))
+
+
+def generate_y(m, factors_table):
+    return [[round(func(row[0], row[1], row[2]) + random.randint(-5, 5), 3) for _ in range(m)] for row in factors_table]
+
+
+def print_matrix(m, N, factors, y_vals, additional_text=":"):
+    labels_table = list(map(lambda x: x.ljust(10),
+                            ["x1", "x2", "x3", "x12", "x13", "x23", "x123", "x1^2", "x2^2", "x3^2"] + [
+                                "y{}".format(i + 1) for i in range(m)]))
+    rows_table = [list(factors[i]) + list(y_vals[i]) for i in range(N)]
+    print("\nМатриця планування" + additional_text)
+    print(" ".join(labels_table))
+    print("\n".join([" ".join(map(lambda j: "{:<+10}".format(j), rows_table[i])) for i in range(len(rows_table))]))
+    print("\t")
+
+
+def print_equation(coeffs, importance=[True] * 11):
+    x_i_names = list(compress(["", "x1", "x2", "x3", "x12", "x13", "x23", "x123", "x1^2", "x2^2", "x3^2"], importance))
+    coefficients_to_print = list(compress(coeffs, importance))
+    equation = " ".join(
+        ["".join(i) for i in zip(list(map(lambda x: "{:+.2f}".format(x), coefficients_to_print)), x_i_names)])
+    print("Рівняння регресії: y = " + equation)
+
+
+def set_factors_table(factors_table):
+    def x_i(i):
+        with_null_factor = list(map(lambda x: [1] + x, generate_factors_table(factors_table)))
+        res = [row[i] for row in with_null_factor]
+        return numpy.array(res)
+
+    return x_i
+
+
+def m_ij(*arrays):
+    return numpy.average(reduce(lambda accum, el: accum * el, list(map(lambda el: numpy.array(el), arrays))))
+
+
+def find_coefficients(factors, y_vals):
+    x_i = set_factors_table(factors)
+    coeffs = [[m_ij(x_i(column), x_i(row)) for column in range(11)] for row in range(11)]
+    y_numpy = list(map(lambda row: numpy.average(row), y_vals))
+    free_values = [m_ij(y_numpy, x_i(i)) for i in range(11)]
+    beta_coefficients = numpy.linalg.solve(coeffs, free_values)
+    return list(beta_coefficients)
+
+
+def cochran_criteria(m, N, y_table):
+    def get_cochran_value(f1, f2, q):
+        partResult1 = q / f2
+        params = [partResult1, f1, (f2 - 1) * f1]
+        fisher = f.isf(*params)
+        result = fisher / (fisher + (f2 - 1))
+        return Decimal(result).quantize(Decimal('.0001')).__float__()
+
+    print("\nПеревірка за критерієм Кохрена: m = {}, N = {}".format(m, N))
+    y_variations = [numpy.var(i) for i in y_table]
+    max_y_variation = max(y_variations)
+    gp = max_y_variation / sum(y_variations)
+    f1 = m - 1
+    f2 = N
+    p = 0.95
+    q = 1 - p
+    gt = get_cochran_value(f1, f2, q)
+    print("Gp = {}; Gt = {}; f1 = {}; f2 = {}; q = {:.2f}".format(gp, gt, f1, f2, q))
+    if gp < gt:
+        print("Gp < Gt => дисперсії рівномірні => все правильно")
+        return True
+    else:
+        print("Gp > Gt => дисперсії нерівномірні => змінюємо значення m")
+        return False
+
+
+def student_criteria(m, N, y_table, beta_coefficients):
+    def get_student_value(f3, q):
+        return Decimal(abs(t.ppf(q / 2, f3))).quantize(Decimal('.0001')).__float__()
+
+    print("\nПеревірка за критерієм Стьюдента: m = {}, N = {} ".format(m, N))
+    average_variation = numpy.average(list(map(numpy.var, y_table)))
+    variation_beta_s = average_variation / N / m
+    standard_deviation_beta_s = math.sqrt(variation_beta_s)
+    t_i = [abs(beta_coefficients[i]) / standard_deviation_beta_s for i in range(len(beta_coefficients))]
+    f3 = (m - 1) * N
+    q = 0.05
+    t_our = get_student_value(f3, q)
+    importance = [True if el > t_our else False for el in list(t_i)]
+
+    print("Оцінки коефіцієнтів βs: " + ", ".join(list(map(lambda x: str(round(float(x), 3)), beta_coefficients))))
+    print("Коефіцієнти ts: " + ", ".join(list(map(lambda i: "{:.2f}".format(i), t_i))))
+    print("f3 = {}; q = {}; tтабл = {}".format(f3, q, t_our))
+    beta_i = ["β0", "β1", "β2", "β3", "β12", "β13", "β23", "β123", "β11", "β22", "β33"]
+    importance_to_print = ["важливий" if i else "неважливий" for i in importance]
+    to_print = map(lambda x: x[0] + " " + x[1], zip(beta_i, importance_to_print))
+    print(*to_print, sep="; ")
+    print_equation(beta_coefficients, importance)
+    return importance
+
+
+def fisher_criteria(m, N, d, x_table, y_table, b_coefficients, importance):
+    def get_fisher_value(f3, f4, q):
+        return Decimal(abs(f.isf(q, f4, f3))).quantize(Decimal('.0001')).__float__()
+
+    f3 = (m - 1) * N
+    f4 = N - d
+    q = 0.05
+    theoretical_y = numpy.array([equation_of_regression(row[0], row[1], row[2], b_coefficients) for row in x_table])
+    average_y = numpy.array(list(map(lambda el: numpy.average(el), y_table)))
+    s_ad = m / (N - d) * sum((theoretical_y - average_y) ** 2)
+    y_variations = numpy.array(list(map(numpy.var, y_table)))
+    s_v = numpy.average(y_variations)
+    f_p = float(s_ad / s_v)
+    f_t = get_fisher_value(f3, f4, q)
+    theoretical_values_to_print = list(
+        zip(map(lambda x: "x1 = {0[1]:<10} x2 = {0[2]:<10} x3 = {0[3]:<10}".format(x), x_table), theoretical_y))
+    print("\nПеревірка за критерієм Фішера: m = {}, N = {} для таблиці y_table".format(m, N))
+    print("Теоретичні значення Y для різних комбінацій факторів:")
+    print("\n".join(["{arr[0]}: y = {arr[1]}".format(arr=el) for el in theoretical_values_to_print]))
+    print("Fp = {}, Ft = {}".format(f_p, f_t))
+    print("Fp < Ft => модель адекватна" if f_p < f_t else "Fp > Ft => модель неадекватна")
+    return True if f_p < f_t else False
+
+
+m = 3
+N = 15
+natural_plan = generate_factors_table(natur_plan_raw)
+y_arr = generate_y(m, natur_plan_raw)
+while not cochran_criteria(m, N, y_arr):
+    m += 1
+    y_arr = generate_y(m, natural_plan)
+
+print_matrix(m, N, natural_plan, y_arr, " для натуралізованих факторів:")
+coefficients = find_coefficients(natural_plan, y_arr)
+print_equation(coefficients)
+importance = student_criteria(m, N, y_arr, coefficients)
+d = len(list(filter(None, importance)))
+fisher_criteria(m, N, d, natural_plan, y_arr, coefficients, importance)
